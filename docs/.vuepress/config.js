76ff5079ba187c0b6b6ddec450e47785b02c6f6f
Taoning Ge <tony.bazhang@gmail.com>
2020-09-25 23:34:20 +0800

    Init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: "火山PC开发平台文档",
+  description: "火山PC文档",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  base: "/",
+  markdown: {
+    lineNumbers: false,
+  },
+  themeConfig: {
+    repo: "tonnyge/voldpc",
+    repoLabel: "GitHub",
+    editLinkText: "在 GitHub 上编辑此页",
+    smoothScroll: true,
+    sidebarDepth: 2,
+    lastUpdated: "上次更新",
+    navbar: true,
+    nav: [
+      {
+        text: "入门",
+        link: "/starter/",
+      },
+    ],
+    sidebar: {
+      "/starter/": getStarterSidebar("入门"),
+    },
+  },
+};
+
+function getStarterSidebar(groupA) {
+  return [
+    {
+      title: groupA,
+      collapsable: false,
+      children: [""],
+    },
+  ];
+}
