734c76e8b8edee53fb5193898c5d538b30da06cc
Taoning Ge <tony.bazhang@gmail.com>
2020-09-30 13:57:49 +0800

    enable PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,36 @@
 module.exports = {
   title: "火山PC开发平台文档",
   description: "火山PC文档",
-  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  head: [
+    ["link", { rel: "icon", href: "/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" },
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" },
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/icons/safari-pinned-tab.svg",
+        color: "#3eaf7c",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content: "/icons/msapplication-icon-144x144.png",
+      },
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+  ],
   base: "/",
   plugins: [
     [
@@ -14,20 +43,20 @@ module.exports = {
     ["@vuepress/back-to-top"],
     ["@vuepress/medium-zoom", true],
     ["@vuepress/nprogress"],
+    ["@vuepress/google-analytics", { ga: "UA-179290022-1" }],
     [
-      "@vuepress/google-analytics",
+      "@vuepress/pwa",
       {
-        ga: "UA-179290022-1", // UA-00000000-0
-      },
-    ],
+        serviceWorker: true,
+        updatePopup: true,
+      }
+    ]
   ],
-  markdown: {
-    lineNumbers: false,
-  },
+  markdown: { lineNumbers: false },
   themeConfig: {
     repo: "tonnyge/voldpc",
     repoLabel: "GitHub",
-    docsDir: 'docs',
+    docsDir: "docs",
     editLinks: true,
     editLinkText: "在 GitHub 上编辑此页",
     smoothScroll: true,
